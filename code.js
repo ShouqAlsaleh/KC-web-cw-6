bf4bfeb137132bcada2f3adf5b7bd9ad86f414c2
ShouqAlsaleh <109864142+ShouqAlsaleh@users.noreply.github.com>
2022-08-17 15:38:25 +0300

    CW6

---
+++ code.js
@@ -0,0 +1,29 @@
+alert (`Welcom, my website will calculate yuor score`)
+
+let grade = ( prompt`write your score `)
+
+console.log(grade)
+
+if ( grade >= 90 && grade <= 100)
+{console.log(`%c لقد حصلت على امتياز 🥳`,`#cdb4db`)}
+
+else if (grade > 80 && grade <   89)
+ {console.log (`لقد حصلت على جيد جداً🤩`)}
+
+ else if (grade > 70 && grade <= 79)
+ {console.log (`لقد حصلت على جيد🙂`)}
+
+
+
+ else if (grade > 60 && grade < 69)
+ {console.log (` لقد حصلت على مقبول😕`)   }
+
+
+
+ else if (grade > 50 && grade < 59)
+ {console.log (`لقد حصلت على ضعيف☹️`)}
+
+ else if(grade == 50 )
+ { console.log(`راسب💔`)}
+
+// JavaScript not Java
