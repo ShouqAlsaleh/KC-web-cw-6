5235c3502f3f0376ac38063e1af66ae63a1976ec
ShouqAlsaleh <109864142+ShouqAlsaleh@users.noreply.github.com>
2022-08-18 12:42:08 +0300

    cw6

--- code.js
+++ code.js
@@ -7,7 +7,7 @@ console.log(grade)
 if ( grade >= 90 && grade <= 100)
 {console.log(`%c لقد حصلت على امتياز 🥳`,`#cdb4db`)}
 
-else if (grade > 80 && grade <   89)
+else if (grade > 80 && grade <=   89)
  {console.log (`لقد حصلت على جيد جداً🤩`)}
 
  else if (grade > 70 && grade <= 79)
@@ -15,15 +15,15 @@ else if (grade > 80 && grade <   89)
 
 
 
- else if (grade > 60 && grade < 69)
+ else if (grade > 60 && grade <= 69)
  {console.log (` لقد حصلت على مقبول😕`)   }
 
 
 
- else if (grade > 50 && grade < 59)
+ else if (grade > 50 && grade <= 59)
  {console.log (`لقد حصلت على ضعيف☹️`)}
 
- else if(grade == 50 )
+ else if(grade <= 50 )
  { console.log(`راسب💔`)}
 
 // JavaScript not Java
