170087166f0606147508697d1c6619afe0d7f52d
ShouqAlsaleh <109864142+ShouqAlsaleh@users.noreply.github.com>
2022-08-20 01:22:27 +0300

    cw6

--- code.js
+++ code.js
@@ -5,25 +5,25 @@ let grade = ( prompt`write your score `)
 console.log(grade)
 
 if ( grade >= 90 && grade <= 100)
-{console.log(`%c لقد حصلت على امتياز 🥳`,`#cdb4db`)}
+{console.log(`%c لقد حصلت على امتياز 🥳`,"color : green")}
 
 else if (grade >= 80 && grade <=   89)
- {console.log (`لقد حصلت على جيد جداً🤩`)}
+ {console.log (`%cلقد حصلت على جيد جداً🤩`,"color : blue")}
 
  else if (grade >= 70 && grade <= 79)
- {console.log (`لقد حصلت على جيد🙂`)}
+ {console.log (`%c لقد حصلت على جيد🙂`,"color : orange")}
 
 
 
  else if (grade >= 60 && grade <= 69)
- {console.log (` لقد حصلت على مقبول😕`)   }
+ {console.log (`%c لقد حصلت على مقبول😕`,"color : yellow")}
 
 
 
  else if (grade >= 50 && grade <= 59)
- {console.log (`لقد حصلت على ضعيف☹️`)}
+ {console.log (`%cلقد حصلت على ضعيف☹️`,"color: red")}
 
  else if(grade < 50 )
- { console.log(`راسب💔`)}
+ { console.log(`%c راسب💔`,"color : grey")}
 
 // JavaScript not Java
